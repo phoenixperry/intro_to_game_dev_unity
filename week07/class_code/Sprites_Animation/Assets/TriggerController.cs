32ab344f4603fba1b0cb5be4ee9f5bb2667a9941
Phoenix Perry <contact@phoenixperry.com>
2016-03-07 16:26:03 +0000

    adding code from class and notes

---
+++ week07/class_code/Sprites_Animation/Assets/TriggerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerController : MonoBehaviour {
+	Animator zombieAnimator; 
+
+	// Use this for initialization
+	void Start () {
+		zombieAnimator = gameObject.GetComponent<Animator> (); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.A)) {
+			zombieAnimator.SetTrigger ("spin"); 
+		}
+	}
+}
